b92f208b8c8f6afacc884a737057bb4ccb8820f3
Anguel <angel.hristozov@gmail.com>
2018-06-28 21:42:08 +0100

    the word of the day is now updated via the script instead of just saving a counter. done to prevent excessive calling from outside source

--- update_word.py
+++ update_word.py
@@ -7,15 +7,36 @@ cred = credentials.Certificate('firebase.json')
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
+# make a reference to the collection
 words_ref = db.collection(u'words')
 
+# see the total amount of words
 totalNumber = words_ref.document(u'numberOfWords').get().to_dict().get(u'numberOfWords')
 
-previousWord = words_ref.document(u'wordOfTheDay').get().to_dict().get(u'wordOfTheDay')
+# get the counter
+counter = words_ref.document(u'wordOfTheDayCounter').get().to_dict().get(u'wordOfTheDayCounter')
 
-if previousWord == totalNumber:
-    previousWord = -1
+# check to see if the counter is over the limit
+if counter == totalNumber:
+    counter = -1
 
+# now get the word that the counter is pointing to in the form { word : definition }
+word_dict = words_ref.document(str(counter)).get().to_dict()
+
+# now separate the dictionary. there will always be one pair. no more. no less.
+word = None
+definition = None
+for key, value in word_dict.items():
+    word = key
+    definition = value
+
+# now save the word and its definition into a separate entry in the database. this is to prevent the app from making more than 1 call
 words_ref.document(u'wordOfTheDay').set({
-    'wordOfTheDay': previousWord + 1
+    'word': word,
+    'definition': definition
+})
+
+# finally update the counter to point to the new word for the following day
+words_ref.document(u'wordOfTheDayCounter').set({
+    'wordOfTheDayCounter': counter + 1
 })
